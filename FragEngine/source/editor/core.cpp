60a083978aba548fe2417d438df42b4779d5a5f8
A55adon <simonwa08@gmx.de>
2026-05-06 19:47:34 +0200

    Added ui beginnings for editor

--- FragEngine/source/editor/core.cpp
+++ FragEngine/source/editor/core.cpp
@@ -14,10 +14,51 @@ void setupUI()
 	editorUI = editorEngine->createNewUI();
 	editorUI->setDebugDraw(true);
 
+	vec2<float> viewportSize = { 70,70 }; //US
+
+	////// Left side
+
+	//// Hirarchy Panel
 	Transform hirarchyPanelTr;
-	hirarchyPanelTr.setPositionUS({ 7.5f, 50 });
-	hirarchyPanelTr.setSizeUS({ 15, 100 });
+	hirarchyPanelTr.setSizeUS({ (100.f - viewportSize.x) / 2.f, 70.f });
+	hirarchyPanelTr.setPositionUS({ (100.f - viewportSize.x) / 4.f, 100.f - hirarchyPanelTr.getSizeUS().y / 2.f });
 	Rectangle* hirarchyPanel = editorUI->createRectangle(hirarchyPanelTr, editorUIStyle);
+
+	// Add Object Btn
+	std::unique_ptr<Button> addObjBtn = std::make_unique<Button>();
+	addObjBtn->setStyle(editorUIStyle);
+	addObjBtn->setOnClick([]() { LOG("test"); });
+	Transform addObjBtnTr;
+	addObjBtnTr.setPositionUS({50.f, 50.f});
+	addObjBtnTr.setSizeUS({10.f,10.f});
+	addObjBtn->setTransform(addObjBtnTr);
+	addObjBtn->setFont(editorUI->getDefaultFont(18));
+	addObjBtn->setLabel("+");
+	addObjBtn->rebuild();
+	hirarchyPanel->addChild(std::move(addObjBtn));
+
+	//// SceneList
+	Transform sceneListPanelTr;
+	sceneListPanelTr.setSizeUS({ (100.f - viewportSize.x) / 2.f, 100.f - hirarchyPanelTr.getSizeUS().y});
+	sceneListPanelTr.setPositionUS({ (100.f - viewportSize.x) / 4.f, sceneListPanelTr.getSizeUS().y / 2.f});
+	Rectangle* sceneListPanel = editorUI->createRectangle(sceneListPanelTr, editorUIStyle);
+
+	////// Right side
+	
+	//// Inspector
+	Transform inspectorPanelTr;
+	inspectorPanelTr.setSizeUS({ (100.f - viewportSize.x) / 2.f, 100.f });
+	inspectorPanelTr.setPositionUS({ 100.f - (inspectorPanelTr.getSizeUS().x / 2.f), inspectorPanelTr.getSizeUS().y / 2.f });
+	Rectangle* inspectorPanel = editorUI->createRectangle(inspectorPanelTr, editorUIStyle);
+	
+	////// Top side
+	
+	//// Stats
+	Transform statsPanelTr;
+	statsPanelTr.setSizeUS({ viewportSize.x, (100.f - viewportSize.y) / 2.f });
+	statsPanelTr.setPositionUS({ 100.f / 2.f, 100.f - statsPanelTr.getSizeUS().y / 2.f});
+	Rectangle* statsPanel = editorUI->createRectangle(statsPanelTr, editorUIStyle);
+	
 }
 
 void startEditor()
@@ -32,17 +73,41 @@ void startEditor()
 	Renderer::get()->setCustomResolution({1200, 800});
 
 	editorCamera = editorGraphics->initNewCamera("EditorCamera", editorWindow->getAspect());
+	editorCamera->setPosition(vec3<float>(0,0,5.f));
 
 	editorLight = editorGraphics->initNewLight("EditorLight");
-	editorLight->position = vec3<float>(0.f, 2.f, 0.f);
+	editorLight->position = vec3<float>(0.f, 2.f, 3.f);
+	editorLight->emissionStrength = 5.f;
 
 	setupUI();
+
+	PhysicsSettings cfg;
+	cfg.isStatic = true;
+
+	SceneObject* defaultObject = editorEngine->createNewSceneObject();
+	defaultObject->getRenderMesh().createCube();
+	defaultObject->getRenderMesh().setColor(Color::Blue);
+	defaultObject->setPosition(vec3<float>(0, 0, 0));
+	defaultObject->setScale(vec3<float>(1.f, 1.f, 1.f));
+	defaultObject->initPhysics(cfg);
+	
+
+	Scene* defaultScene = editorEngine->saveToNewScene("defaultScene");
+	int defaultSceneID = defaultScene->getID();
+
+	editorEngine->loadSceneByID(defaultSceneID);
 }
 
 void updateEditor(float dt)
 {
 	for (auto& o : editorEngine->loadedScene.getAllObjects().getAll())
 		o->syncFromPhysics();
+
+	GLFWwindow* win = editorWindow->getRawWindow();
+	double mx, my;
+	glfwGetCursorPos(win, &mx, &my);
+	bool lmbDown = glfwGetMouseButton(win, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+	editorUI->update(static_cast<float>(mx), static_cast<float>(my), lmbDown);
 }
 
 void drawEditor() 
